e233a0dfd8d7d8fa196e9767ed5838900a72801e
Monish Karwal <monish1apr@gmail.com>
2020-02-23 14:06:08 +0530

    User register module

--- server/models/user.js
+++ server/models/user.js
@@ -4,7 +4,46 @@ TODO:
     2. User model methods (setPassword, validatePassword, generateJWT)
     3. Password hashing
     4. Crypto module
-    5. Config file
-    6. Joi validation
+    5. Joi validation
 
 */
+
+const mongoose = require("mongoose");
+const Joi = require("Joi");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    email: { type: String, unique: true },
+    password: String,
+    createdAt: { type: Date, default: Date.now }
+});
+
+const User = mongoose.model("User", userSchema);
+
+function validate(userData) {
+    const joiSchema = Joi.object({
+        firstName: Joi.string()
+            .min(3)
+            .max(15)
+            .required(),
+        lastName: Joi.string()
+            .min(3)
+            .max(15)
+            .required(),
+        email: Joi.string()
+            .email()
+            .required(),
+        password: Joi.string()
+            .min(5)
+            .max(20)
+            .required()
+    });
+    return Joi.validate(userData, joiSchema);
+}
+
+userSchema.methods.setPassword = password => {};
+
+module.exports = { User, validate };
--- server/routes/register.js
+++ server/routes/register.js
@@ -1,8 +1,35 @@
 const express = require("express");
 const Router = express.Router();
 
+const { User, validate } = require("../models/user");
+
 Router.get("/", (req, res) => {
     res.send("Register End Point");
+});
+
+Router.post("/", async (req, res) => {
+    const result = validate(req.body);
+
+    const { value, error } = result;
+
+    if (error) {
+        res.json({
+            errorType: error.name,
+            errorMessage: error.details[0].message
+        });
+    }
+
+    let user = await User.findOne({ email: req.body.email });
+
+    if (user) {
+        res.json({
+            message: "User already exists!"
+        });
+    } else {
+        user = new User(value);
+        await user.save();
+        res.send(user);
+    }
 });
 
 module.exports = Router;
